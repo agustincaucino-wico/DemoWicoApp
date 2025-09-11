3d03e5709dadf8d2b2baeb362e20b1112815e7ba
Federico Covelo <128938535+TheDamage@users.noreply.github.com>
2025-09-10 23:52:17 -0300

    Fix asgi.py

--- app/myapp/asgi.py
+++ app/myapp/asgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), '..', '.env')
+load_dotenv(dotenv_path)
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myapp.settings.prod")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE")
 
 application = get_asgi_application()
