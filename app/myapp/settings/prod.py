40dd3f35a988635555f8c7695979242956e98922
Federico Covelo <128938535+TheDamage@users.noreply.github.com>
2025-09-16 13:44:17 -0300

    Update prod.py

--- app/myapp/settings/prod.py
+++ app/myapp/settings/prod.py
@@ -9,7 +9,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["back-appmobile-tst.wico.com.ar", "localhost", "host.docker.internal"]
+ALLOWED_HOSTS = ["back-appmobile-tst.wico.com.ar", "localhost", "172.31.25.8"]
+
 # TO DO - el ultimo es probablemente un health check que se hace en el server, cambiar cuando
 # se implemente nginx
 
