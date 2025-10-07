62dc86e56d460ff3807bb0174c9ab4fc7c1b58de
bruno-wico <bruno.spoletini@wico.com.ar>
2025-10-07 13:59:15 -0300

    Sitio de admin corregido para incluir las estaciones

--- app/stations/admin.py
+++ app/stations/admin.py
@@ -1,17 +1,22 @@
 from django.contrib import admin
+from myapp.admin import my_admin_site
 
 from .models import Station, StationAttendantAssignment
 
 
-@admin.register(Station)
+@admin.register(Station, site=my_admin_site)
 class StationAdmin(admin.ModelAdmin):
     list_display = ("name", "province", "city", "street", "street_number")
     list_filter = ("province", "city")
     search_fields = ("name", "street", "city__name", "province__name")
 
 
-@admin.register(StationAttendantAssignment)
+@admin.register(StationAttendantAssignment, site=my_admin_site)
 class StationAttendantAssignmentAdmin(admin.ModelAdmin):
     list_display = ("attendant", "station", "start_date", "end_date")
     list_filter = ("station", "start_date", "end_date")
-    search_fields = ("attendant__email", "attendant__first_name", "attendant__last_name")
+    search_fields = (
+        "attendant__email",
+        "attendant__first_name",
+        "attendant__last_name",
+    )
