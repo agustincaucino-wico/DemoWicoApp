565a11ed5c53444f8dfac58d2a0cecca9dbfbfb9
bruno-wico <bruno.spoletini@wico.com.ar>
2025-11-07 10:45:30 -0300

    Cambio en el model para que las cuentas se borren cuando se borran los usuarios

--- app/accounts/models.py
+++ app/accounts/models.py
@@ -8,7 +8,7 @@ from django.core.exceptions import ValidationError
 class Account(models.Model):
     ACCOUNT_TYPES = [("holder", "Titular"), ("dependent", "Adherido")]
 
-    user = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True)
     balance = models.DecimalField(max_digits=12, decimal_places=2)
     account_type = models.CharField(max_length=20, choices=ACCOUNT_TYPES)
     created_at = models.DateTimeField(auto_now_add=True)
