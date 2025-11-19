7867240eff771885af102396513fd1ffb8d332ba
bruno-wico <bruno.spoletini@wico.com.ar>
2025-11-19 09:58:16 -0300

    Cambio en las settings de prod

--- app/myapp/settings/prod.py
+++ app/myapp/settings/prod.py
@@ -33,8 +33,18 @@ DATABASES = {
         "PASSWORD": os.getenv("DATABASE_PASSWORD"),
         "HOST": os.getenv("DATABASE_HOST"),
         "PORT": os.getenv("DATABASE_PORT"),
-    }
+    },
+    "prod": {
+        "ENGINE": "django.db.backends.postgresql",
+        "OPTIONS": {"options": "-c search_path=wicoapp,public"},
+        "NAME": os.getenv("DATABASE_NAME"),
+        "USER": os.getenv("DATABASE_USER"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD"),
+        "HOST": os.getenv("DATABASE_HOST"),
+        "PORT": os.getenv("DATABASE_PORT"),
+    },
 }
+
 
 STATIC_URL = "/static/"
 STATIC_ROOT = "/appContainer/staticfiles"
