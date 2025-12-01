a958e8507786cbc7e2cd3a0f34768aeb9acac11b
bruno-wico <bruno.spoletini@wico.com.ar>
2025-12-01 10:34:08 -0300

    Config prod - cambio a schema public

--- app/myapp/settings/prod.py
+++ app/myapp/settings/prod.py
@@ -26,12 +26,12 @@ MIDDLEWARE = [
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "OPTIONS": {"options": "-c search_path=wicoapp,public"},
         "NAME": os.getenv("DATABASE_NAME"),
         "USER": os.getenv("DATABASE_USER"),
         "PASSWORD": os.getenv("DATABASE_PASSWORD"),
         "HOST": os.getenv("DATABASE_HOST"),
         "PORT": os.getenv("DATABASE_PORT"),
+        "OPTIONS": {"options": "-c search_path=public"},
     },
 }
 
