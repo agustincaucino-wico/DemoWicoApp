3469359d7f174ec7d22bc3832e1d6b649427b201
bruno-wico <bruno.spoletini@wico.com.ar>
2025-12-18 09:58:55 -0300

    Borrado de comentarios

--- app/myapp/settings/dev.py
+++ app/myapp/settings/dev.py
@@ -12,7 +12,6 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 ALLOWED_HOSTS = [
     "back-appmobile-tst.wico.com.ar",
     "172.31.25.8",
-    # etc
 ]
 
 ROOT_URLCONF = "myapp.urls"
