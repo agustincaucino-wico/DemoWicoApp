478c0db0197dbd70ddabcbf03606f4e9b2dd8456
bruno-wico <bruno.spoletini@wico.com.ar>
2025-12-18 10:49:50 -0300

    Endpoint / ahora retorna la view del health check

--- app/myapp/urls.py
+++ app/myapp/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import RedirectView
 from users.admin import my_admin_site
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -10,9 +9,7 @@ from rest_framework_simplejwt.views import (
 from .views import HealthCheckView, AdminSpectacularAPIView, AdminSpectacularSwaggerView
 
 urlpatterns = [
-    path(
-        "", RedirectView.as_view(url="/health/", permanent=False), name="root_redirect"
-    ),
+    path("", HealthCheckView.as_view(), name="root"),
     path("admin/", my_admin_site.urls),
     path("users/", include("users.urls")),
     path("locations/", include("locations.urls")),
