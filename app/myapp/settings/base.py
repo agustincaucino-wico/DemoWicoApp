4aef87b4e8a8f4fa431b114cd1052a69a6d0725b
bruno-wico <bruno.spoletini@wico.com.ar>
2026-02-13 10:21:55 -0300

    Cambio de version

--- app/myapp/settings/base.py
+++ app/myapp/settings/base.py
@@ -6,8 +6,8 @@ from datetime import timedelta
 load_dotenv()
 
 # Versión actual de la API
-API_VERSION = "1.1.10"
-MIN_APP_VERSION = "1.1.6"
+API_VERSION = "1.1.15"
+MIN_APP_VERSION = "1.1.9"
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
