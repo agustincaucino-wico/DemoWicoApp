3ab27b2325d1af99a5bd34ea93a791ce47b9801b
bruno-wico <bruno.spoletini@wico.com.ar>
2026-02-27 11:50:52 -0300

    Cambio de version maxima

--- app/myapp/settings/base.py
+++ app/myapp/settings/base.py
@@ -6,7 +6,7 @@ from datetime import timedelta
 load_dotenv()
 
 # Versión actual de la API
-API_VERSION = "1.1.16"
+API_VERSION = "1.1.20"
 MIN_APP_VERSION = "1.1.15"
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
